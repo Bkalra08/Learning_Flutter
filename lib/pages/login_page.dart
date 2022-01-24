fb86777d9b099df2eb25631d65e678a5f8c172c5
Bhavya Kalra <bkalra08032002@gmail.com>
2022-01-24 20:51:31 +0530

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,12 +4,68 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Center(
-            child: Text("Login Page",
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                ))));
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150.0,
+            ),
+            Image.asset(
+              "assets/images/images (2).jpg",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+
+            // TextFormField(
+            //   decoration: InputDecoration(
+            //     hintText: "Enter UserName ",
+            //     labelText: "UserName",
+            //   ),
+            // )
+            Text(
+              "The point is DID SHE?",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username:",
+                        labelText: "User ka naam",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter password:",
+                        labelText: "User ka password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("No , She Didn't😂😂🤣🤣");
+                      },
+                    )
+                  ],
+                ))
+          ],
+        ));
   }
 }
