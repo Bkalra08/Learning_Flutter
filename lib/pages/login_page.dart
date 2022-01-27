6d1bfe62670c0448199c8b24a880e06c6a5693ec
Bhavya Kalra <bkalra08032002@gmail.com>
2022-01-27 23:50:34 +0530

    Day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,44 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
+class loginpage extends StatefulWidget {
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
 
-class loginpage extends StatelessWidget {
+class _loginpageState extends State<loginpage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 150.0,
-            ),
-            Image.asset(
-              "assets/images/images (2).jpg",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 100.0,
-            ),
-
-            // TextFormField(
-            //   decoration: InputDecoration(
-            //     hintText: "Enter UserName ",
-            //     labelText: "UserName",
-            //   ),
-            // )
-            Text(
-              "The point is DID SHE?",
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150.0,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
+              Image.asset(
+                "assets/images/images (2).jpg",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+
+              // TextFormField(
+              //   decoration: InputDecoration(
+              //     hintText: "Enter UserName ",
+              //     labelText: "UserName",
+              //   ),
+              // )
+              Text(
+                "The point is DID SHE? Welcome $name",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
                 padding: const EdgeInsets.symmetric(
                     vertical: 16.0, horizontal: 32.0),
                 child: Column(
@@ -48,6 +55,10 @@ class loginpage extends StatelessWidget {
                         hintText: "Enter Username:",
                         labelText: "User ka naam",
                       ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -59,20 +70,49 @@ class loginpage extends StatelessWidget {
                     SizedBox(
                       height: 20.0,
                     ),
-                    ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                    )
+                    // ElevatedButton(
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    // )
+                    InkWell(
+                        onTap: () async {
+                          setState(() {
+                            changeButton = true;
+                          });
+                          await Future.delayed(Duration(seconds: 1));
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 100,
+                          height: 40,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(Icons.done, color: Colors.white)
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            // shape: changeButton
+                            //     ? BoxShape.circle
+                            //     : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 20 : 10),
+                          ),
+                        )),
                   ],
                 ),
-            )
-        
-          ],
-        ),
-        )
-        );
+              )
+            ],
+          ),
+        ));
   }
 }
